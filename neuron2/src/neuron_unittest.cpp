51484b54f67a5c6f9f894748eb38e5d4a1b250da
laurinelang <laurine.lang@epfl.ch>
2017-10-19 19:06:24 +0200

    supress comments

--- neuron2/src/neuron_unittest.cpp
+++ neuron2/src/neuron_unittest.cpp
@@ -7,7 +7,6 @@ TEST (neuronTest, Test_mPotential)
 {
 	Neuron neuron;
 	neuron.update(1, 1.0);
-	//EXPECT_EQ(20.0*(1.0-std::exp(-0.1/20.0)),neuron.getMembranePotential());
 	EXPECT_NEAR(20.0*(1.0-std::exp(-0.1/20.0)),neuron.getMembranePotential(), 10e-5);
 }
 
@@ -18,9 +17,6 @@ TEST (neuronTest, Test_spikesTimes)
 	{
 		neuron.update(i, 1.01);
 	}
-	//EXPECT_EQ(192.4, neuron.getSpikesTimes()[0]*H);
-	//EXPECT_EQ(286.8, neuron.getSpikesTimes()[1]*H);
-	//EXPECT_EQ(381.2, neuron.getSpikesTimes()[2]*H);
 	EXPECT_NEAR(192.4, neuron.getSpikesTimes()[0]*H, 10e-5);
 	EXPECT_NEAR(286.8, neuron.getSpikesTimes()[1]*H, 10e-5);
 	EXPECT_NEAR(381.2, neuron.getSpikesTimes()[2]*H, 10e-5);
